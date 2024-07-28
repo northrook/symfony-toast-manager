eb201f5c93718832d365cac06f7d432502ba42f5
Martin <hello@northrook.com>
2024-07-28 10:07:35 +0100

    mend

--- src/ToastService.php
+++ src/ToastService.php
@@ -5,6 +5,7 @@ declare( strict_types = 1 );
 namespace Northrook\Symfony\Service;
 
 use Northrook\Symfony\Service\ToastService\Message;
+use Stringable;
 use Symfony\Component\HttpFoundation as Http;
 use Symfony\Component\HttpFoundation\Exception\SessionNotFoundException;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
@@ -20,6 +21,19 @@ final readonly class ToastService
         return $this->flashBag()->get( $key )[ 0 ] ?? null;
     }
 
+    /**
+     * Adds a simple flash message to the current session.
+     *
+     * @param string                   $type  = ['info', 'success', 'warning', 'error', 'notice'][$any]
+     * @param string|Stringable|array  $message
+     *
+     * @return $this
+     */
+    public function flash( string $type, string | Stringable | array $message ) : ToastService {
+        $this->flashBag()->add( $type, $message );
+        return $this;
+    }
+
     public function message(
         string  $type,
         string  $title,
@@ -32,7 +46,7 @@ final readonly class ToastService
 
         if ( $this->flashBag()->has( $flashKey ) ) {
             $message = $this->getFlashBagMessage( $flashKey );
-            $message?->bump($description);
+            $message?->bump( $description );
         }
 
         $message ??= new Message( $type, $title, $description, $timeout );
